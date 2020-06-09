8af1fb04da6005dbe5d475e4d0141b0dd0eab374
Fleshborne <blakeathompson94@gmail.com>
2020-06-09 18:08:31 -0400

    employee code written, next is engineer

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+// class Employee makes sense as the parent class,
+// essentially everyone is an employee so keep it basic
+class Employee {
+  constructor(name, email, id) {
+    this.name = name;
+    this.email = email;
+    this.id = id;
+  }
+  //   write functions to display these constructor traits
+  getName() {
+    return this.name;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getId() {
+    return this.id;
+  }
+  getRole() {
+    return "Employee";
+  }
+}
+module.exports = Employee;
