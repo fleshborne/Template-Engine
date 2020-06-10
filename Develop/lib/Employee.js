420d733521de91390dea48522a853e4de56225ad
Fleshborne <blakeathompson94@gmail.com>
2020-06-09 22:10:02 -0400

    added some comments to better explain workflow for later self looking back

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,6 +1,8 @@
 // TODO: Write code to define and export the Employee class
 // class Employee makes sense as the parent class,
 // essentially everyone is an employee so keep it basic
+// remember that keyword "super" will refer to the parent's class objects
+// cited from : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
 class Employee {
   constructor(name, email, id) {
     this.name = name;
@@ -22,3 +24,5 @@ class Employee {
   }
 }
 module.exports = Employee;
+
+//
