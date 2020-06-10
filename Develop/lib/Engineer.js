3fd1d02f6473ee1a10fe021b83414d553f6166f9
Fleshborne <blakeathompson94@gmail.com>
2020-06-09 21:47:01 -0400

    engineer code written, next is intern

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// require the parent 'employee'
+const Employee = require("./Employee");
+class Engineer extends Employee {
+  constructor(name, id, email, gitHub) {
+    super(name, id, email);
+    this.gitHub = gitHub;
+  }
+  // write functions to call the data
+  getGithub() {
+    return this.gitHub;
+  }
+  // create getRole as described in test file
+  getRole() {
+    return "Engineer";
+  }
+}
+
+module.exports = Engineer;
